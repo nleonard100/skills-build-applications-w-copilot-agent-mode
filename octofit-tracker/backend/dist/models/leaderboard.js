4d3edaf408eab4cd7d94ad52174066d9fc68728f
Ned Leonard <nedleonard100@gmail.com>
2026-08-10 20:50:59 +0000

    backend fix

---
+++ octofit-tracker/backend/dist/models/leaderboard.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+const leaderboardSchema = new mongoose.Schema({
+    rank: { type: Number, required: true, min: 1 },
+    teamId: { type: mongoose.Schema.Types.ObjectId, ref: 'Team', required: true },
+    teamName: { type: String, required: true },
+    score: { type: Number, required: true, min: 0 },
+}, { timestamps: true });
+const LeaderboardModel = mongoose.models.Leaderboard || mongoose.model('Leaderboard', leaderboardSchema);
+export { LeaderboardModel };
